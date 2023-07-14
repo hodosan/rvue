b7e3b6e5f8215d2cac4dd357504d87a56095c070
Your Name <you@example.com>
2023-07-14 02:21:45 +0000

    devise signed_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  
+  before_action :permit_params, if: :devise_controller?
+
+  protected
+
+  def permit_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
